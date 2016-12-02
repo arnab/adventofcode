5dfd47b433c6e9415439a531576ea7942e79adec
Arnab Deka <arnab.deka@gmail.com>
2016-12-02 13:25:52 -0800

    way simpler duplicate detection.

--- src/aoc/extensions/List.kt
+++ src/aoc/extensions/List.kt
@@ -2,18 +2,6 @@ package aoc.extensions
 
 import kotlin.collections.List
 
-fun <E> List<E>.possiblyNegativeLookup(i: Int): E {
-    return if (i < 0) this[this.size + i] else this[i]
-}
+fun <E> List<E>.possiblyNegativeLookup(i: Int): E = if (i < 0) this[this.size + i] else this[i]
 
-fun <E> List<E>.findFirstDuplicate(): E? {
-    val counts = mutableMapOf<E, Int>()
-    this.forEach {
-        if (counts.containsKey(it))
-            counts.put(it, counts[it]!! + 1)
-        else
-            counts.put(it, 1)
-    }
-
-    return this.find { counts[it]!! > 1 }
-}
+fun <E> List<E>.findFirstDuplicate(): E? = HashSet<E>().let { set -> firstOrNull { !set.add(it) } }
