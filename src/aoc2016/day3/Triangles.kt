9342df69c1e0167e177eb8e913846697dfa11248
Arnab Deka <arnab.deka@gmail.com>
2016-12-02 22:19:00 -0800

    vertical groupings.

--- src/aoc2016/day3/Triangles.kt
+++ src/aoc2016/day3/Triangles.kt
@@ -18,4 +18,32 @@ object Triangles {
                 .count { it.isValid() }
     }
 
+    fun countValidVertical(data: String): Int {
+        return data.lines()
+                .map(String::trim)
+                .filter(String::isNotBlank)
+                .map { it.split(Regex("""\s+""")) }
+                .withIndex()
+                .groupBy {
+                    val (index, _) = it
+                    index / 3
+                }
+                .values
+                .toList()
+                .map { it.map { it.value } }
+                .map(this::trianglesFromGroupsOfThree)
+                .flatten()
+                .count { it.isValid() }
+    }
+
+    private fun trianglesFromGroupsOfThree(group: List<List<String>>): List<Triangle> {
+        return listOf(trianglesFromGroups(group, 0), trianglesFromGroups(group, 1), trianglesFromGroups(group, 2))
+                .flatten()
+    }
+
+    private fun trianglesFromGroups(group: List<List<String>>, i: Int): List<Triangle> {
+        return listOf(
+                Triangle(group[0][i].toInt(), group[1][i].toInt(), group[2][i].toInt())
+        )
+    }
 }
--- tst/aoc2016/day3/TrianglesTest.kt
+++ tst/aoc2016/day3/TrianglesTest.kt
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test
 internal class TrianglesTest {
     fun countValidTriangles(data: String): Int = Triangles.countValid(data)
     fun countValidTrianglesFromFile(filename: String): Int = countValidTriangles(TestResourceReader.readFile(filename))
+    fun countValidTrianglesVertical(data: String): Int = Triangles.countValidVertical(data)
+    fun countValidTrianglesVerticalFromFile(filename: String): Int = countValidTrianglesVertical(TestResourceReader.readFile(filename))
 
 
     @Test
@@ -31,5 +33,23 @@ internal class TrianglesTest {
     fun countValid_Problem1() {
         val count = countValidTrianglesFromFile("resources/aoc2016/day3/input.txt")
         assertEquals(982, count)
+    }
+
+    @Test
+    fun countValidVertical_Exmaple1() {
+        val data = """101 301 501
+                      102 302 502
+                      103 303 503
+                      201 401 601
+                      202 402 602
+                      203 403 1784"""
+        val count = countValidTrianglesVertical(data)
+        assertEquals(5, count)
+    }
+
+    @Test
+    fun countValidVertical_Problem1() {
+        val count = countValidTrianglesVerticalFromFile("resources/aoc2016/day3/input.txt")
+        assertEquals(1826, count)
     }
 }
