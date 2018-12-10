c74bda525cd9e7738bf60bd35132d4abc778fab3
Arnab Deka <arnab.deka@gmail.com>
2018-12-09 18:57:10 -0800

    day 7 part 2 done

--- src/main/kotlin/aoc2018/day7/Steps.kt
+++ src/main/kotlin/aoc2018/day7/Steps.kt
@@ -1,6 +1,18 @@
 package aoc2018.day7
 
-data class Step(val id: String)
+data class Step(val id: String, val timeToComplete: Int = 0)
+data class Worker(val id: Int, var currentStep: Step? = null, var secondsSpent: Int = 0) {
+
+    fun isWorking() = currentStep != null
+
+    @Synchronized fun assignWork(step: Step?, seconds: Int = 0) {
+        currentStep = step
+        secondsSpent = seconds
+    }
+
+    @Synchronized fun isWorkComplete() = currentStep != null && secondsSpent > currentStep!!.timeToComplete
+
+}
 
 object Steps {
 
@@ -24,6 +36,56 @@ object Steps {
         }
 
         return stepsInOrderOfCompletion
+    }
+
+    fun calculateOrderWithWorkers(steps: List<Pair<Step, Step>>, numWorkers: Int): Pair<List<Step>, Long> {
+        var currentTime: Long = 0
+        val workers = (0..(numWorkers-1)).map { i -> Worker(i) }
+
+        val stepsInOrderOfCompletion = mutableListOf<Step>()
+        val remainingStepsSorted = (steps.map { it.first } + steps.map { it.second }).distinct().sortedBy { it.id }.toMutableList()
+        val totalStepsCount = remainingStepsSorted.size
+
+        val dependencies: MutableMap<Step, MutableSet<Step>> = steps.fold(mutableMapOf()) { deps, (dependentStep, step) ->
+            deps.computeIfAbsent(step) { mutableSetOf() }.add(dependentStep)
+            deps
+        }
+
+        while(stepsInOrderOfCompletion.size < totalStepsCount || workers.any(Worker::isWorking)) {
+            // 1. Update current worker's states
+            workers.filter(Worker::isWorking).forEach { worker ->
+                worker.secondsSpent++
+                if (worker.isWorkComplete()) {
+                    val completedStep = worker.currentStep!!
+                    stepsInOrderOfCompletion.add(completedStep)
+                    dependencies.forEach { _, dependentSteps -> dependentSteps.remove(completedStep) }
+                    worker.assignWork(null)
+                }
+            }
+
+            // 2. Assign new worker to idle workers
+            workers.filterNot(Worker::isWorking).forEach {
+                val nextStep = remainingStepsSorted.firstOrNull { step ->
+                    !dependencies.containsKey(step) || dependencies.getOrDefault(step, mutableSetOf()).isEmpty()
+                }
+                if (nextStep != null) {
+                    remainingStepsSorted.remove(nextStep)
+                    it.assignWork(nextStep, 1)
+                }
+            }
+
+            report(currentTime, workers, stepsInOrderOfCompletion)
+            currentTime++
+        }
+
+        return Pair(stepsInOrderOfCompletion, currentTime - 1)
+    }
+
+    private fun report(currentTime: Long, workers: List<Worker>, stepsInOrderOfCompletion: MutableList<Step>) {
+        val printWorker = { w:Worker -> w.currentStep?.id ?: "." }
+        println("Time: $currentTime " +
+                "Workers: [${workers.map(printWorker).joinToString(" ")}] " +
+                "Completed: ${stepsInOrderOfCompletion.map(Step::id).joinToString("")}")
     }
 
 }
--- src/test/kotlin/aoc2018/day7/StepsTest.kt
+++ src/test/kotlin/aoc2018/day7/StepsTest.kt
@@ -28,18 +28,46 @@ internal class StepsTest {
         Assertions.assertEquals("CABDFE", stepsInOrder.map(Step::id).joinToString(""))
     }
 
-    private fun parseData(data: String): List<Pair<Step, Step>> {
+    @Test
+    fun part2_StepOrderByWorkers_Problem1() {
+        val steps = parseData(TestResourceReader.readFile("aoc2018/day7/input.txt"), baseTimeTaken = 60)
+        val (stepsInOrder, timeTaken) = Steps.calculateOrderWithWorkers(steps, numWorkers = 5)
+        Assertions.assertEquals(1234, timeTaken)
+        Assertions.assertEquals("SCLPAMQVWYNUHODTRGKBJEFXZI", stepsInOrder.map(Step::id).joinToString(""))
+    }
+
+    @Test
+    fun part2_StepOrderByWorkers_Example1() {
+        val steps = parseData("""
+            Step C must be finished before step A can begin.
+            Step C must be finished before step F can begin.
+            Step A must be finished before step B can begin.
+            Step A must be finished before step D can begin.
+            Step B must be finished before step E can begin.
+            Step D must be finished before step E can begin.
+            Step F must be finished before step E can begin.
+        """.trimIndent(), baseTimeTaken = 0)
+        val (stepsInOrder, timeTaken) = Steps.calculateOrderWithWorkers(steps, numWorkers = 2)
+        Assertions.assertEquals("CABFDE", stepsInOrder.map(Step::id).joinToString(""))
+        Assertions.assertEquals(15, timeTaken)
+    }
+
+    private fun parseData(data: String, baseTimeTaken: Int = 0): List<Pair<Step, Step>> {
         return data.split("\n")
                 .filterNot(String::isEmpty)
-                .map { parseDataLine(it) }
+                .map { parseDataLine(it, baseTimeTaken) }
     }
 
     // Matches: Step D must be finished before step E can begin.
     private val dataLineRegex = """Step (\w) must be finished before step (\w) can begin.""".toRegex()
 
-    private fun parseDataLine(line: String): Pair<Step, Step> {
+    private fun parseDataLine(line: String, baseTimeTaken: Int): Pair<Step, Step> {
         val (stepOne, stepTwo)  = dataLineRegex.find(line)!!.destructured
-        return Pair(Step(stepOne), Step(stepTwo))
+        return Pair(Step(stepOne, calculateTimeTaken(stepOne, baseTimeTaken)), Step(stepTwo, calculateTimeTaken(stepTwo, baseTimeTaken)))
+    }
+
+    private fun calculateTimeTaken(stepId: String, baseTimeTaken: Int): Int {
+        return stepId.single().toInt() - ('A'.toInt() - 1) + baseTimeTaken
     }
 
 }
