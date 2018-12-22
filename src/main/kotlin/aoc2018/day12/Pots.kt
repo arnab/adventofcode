f7ce00e0af3b2d22e26cad0ae5a61c84745cd721
Arnab Deka <arnab.deka@gmail.com>
2018-12-21 16:00:30 -0800

    day 12 WIP

---
+++ src/main/kotlin/aoc2018/day12/Pots.kt
@@ -0,0 +1,9 @@
+package aoc2018.day12
+
+data class Pots(val initialState: String, val rules: Map<CharSequence, CharSequence>) {
+
+    fun sumAfterNGenerations(numGenerations: Int): Int {
+
+    }
+
+}
---
+++ src/test/kotlin/aoc2018/day12/PotsTest.kt
@@ -0,0 +1,54 @@
+package aoc2018.day12
+
+import aoc.util.TestResourceReader
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class PotsTest {
+
+    @Test
+    fun part1_Generations_Problem() {
+        val (initialState, rules)  = parseData(TestResourceReader.readFile("aoc2018/day12/input.txt"))
+        TODO("complete")
+    }
+
+    @Test
+    fun part1_Generations_Example() {
+        val (initialState, rules)  = parseData(exampleData())
+        val pots = Pots(initialState, rules)
+        val answer = pots.sumAfterNGenerations(20)
+        assertEquals(325, answer)
+    }
+
+    private fun exampleData() = """
+            initial state: #..#.#..##......###...###
+
+            ...## => #
+            ..#.. => #
+            .#... => #
+            .#.#. => #
+            .#.## => #
+            .##.. => #
+            .#### => #
+            #.#.# => #
+            #.### => #
+            ##.#. => #
+            ##.## => #
+            ###.. => #
+            ###.# => #
+            ####. => #
+        """.trimIndent()
+
+    private fun parseData(data: String): Pair<String,Map<CharSequence, CharSequence>> {
+        val lines = data.split("\n").filterNot(String::isEmpty)
+
+        val initialState = lines.first()
+
+        val rules: Map<CharSequence, CharSequence> = lines.drop(1).associate { line ->
+            val (key, value) = line.split(" => ", limit = 2)
+            key to value
+        }
+        return Pair(initialState, rules)
+    }
+
+}
