9c8dba535f073b487e19d659847d33a4f66bebdf
Arnab Deka <arnab.deka@gmail.com>
2020-12-08 12:56:44 -0800

    ✅ Day 8 part 1 :)

---
+++ src/main/kotlin/aoc2020/day8/InfiniteLoop.kt
@@ -0,0 +1,63 @@
+package aoc2020.day8
+
+import java.lang.IllegalArgumentException
+
+object InfiniteLoop {
+
+    data class Program(
+        val instructions: List<Pair<String, Int>>,
+        val currentState: State
+    ) {
+        companion object {
+            fun parse(rawInstructions: String): Program =
+                rawInstructions.split("\n")
+                    .map { it.parseInstruction() }
+                    .let { instructions -> Program(instructions, State.init()) }
+        }
+
+        data class State(
+            val currentInstruction: Int,
+            val currentAccumulatedValue: Int,
+            val seenInstructions: Set<Int>
+        ) {
+            companion object {
+                fun init() = State(0,0, emptySet())
+            }
+        }
+
+        fun eval(): State = evalRecursively(currentState)
+
+        private fun evalRecursively(state: State): State {
+            val allInstructionsProcessed = state.currentInstruction >= this.instructions.size
+            val instructionAlreadyEvaluated = state.seenInstructions.contains(state.currentInstruction)
+            if (allInstructionsProcessed || instructionAlreadyEvaluated) return state
+
+            val (operator, value) = this.instructions[state.currentInstruction]
+            val nextState = when(operator) {
+                "nop" -> state.copy(
+                    seenInstructions = state.seenInstructions + state.currentInstruction,
+                    currentInstruction = state.currentInstruction + 1
+                )
+
+                "acc" -> state.copy(
+                    seenInstructions = state.seenInstructions + state.currentInstruction,
+                    currentInstruction = state.currentInstruction + 1,
+                    currentAccumulatedValue = state.currentAccumulatedValue + value
+                )
+
+                "jmp" -> state.copy(
+                    seenInstructions = state.seenInstructions + state.currentInstruction,
+                    currentInstruction = state.currentInstruction + value
+                )
+
+                else -> throw IllegalArgumentException("Unknown operator: $operator")
+            }
+            return evalRecursively(nextState)
+        }
+    }
+}
+
+private fun String.parseInstruction() = this.split(" ").let {
+    (operation, value) -> Pair(operation, value.toInt())
+}
+
---
+++ src/test/kotlin/aoc2020/day8/InfiniteLoopTest.kt
@@ -0,0 +1,36 @@
+package aoc2020.day8
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class InfiniteLoopTest {
+    private val exampleInput = """
+            nop +0
+            acc +1
+            jmp +4
+            acc +3
+            jmp -3
+            acc -99
+            acc +1
+            jmp -4
+            acc +6
+        """.trimIndent()
+
+    private val problemInput = File("src/test/resources/aoc2020/day8/input.txt").readText()
+
+    @Test
+    fun `part 1 test`() {
+        val program = InfiniteLoop.Program.parse(exampleInput)
+        val state = program.eval()
+        Assertions.assertEquals(5, state.currentAccumulatedValue)
+    }
+
+    @Test
+    fun `part 1 real`() {
+        val program = InfiniteLoop.Program.parse(problemInput)
+        val state = program.eval()
+        Assertions.assertEquals(1797, state.currentAccumulatedValue)
+    }
+
+}
