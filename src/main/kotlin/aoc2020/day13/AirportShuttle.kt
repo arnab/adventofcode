c88e43dc9d62d0e8ac5667b0bdc1dfdf188932da
Arnab Deka <arnab.deka@gmail.com>
2020-12-25 00:44:18 -0800

    ✅ Day 13 part 2

--- src/main/kotlin/aoc2020/day13/AirportShuttle.kt
+++ src/main/kotlin/aoc2020/day13/AirportShuttle.kt
@@ -6,10 +6,15 @@ object AirportShuttle {
     fun parse(data: String): Pair<Int, List<Bus>> {
         val earliestTime = data.split("\n")[0].toInt()
         val busses = data.split("\n")[1].split(",").mapIndexedNotNull { i, n ->
-            if (n == "x") null else Bus(i, n.toInt())
+            if (n == "x") null else Bus(i, n.toLong())
         }
         return Pair(earliestTime, busses)
     }
+
+    data class Bus(
+        val index: Int,
+        val id: Long
+    )
 
     fun findEarliestPossibleBus(earliestTime: Int, busses: List<Bus>): Pair<Int, Bus>? = busses.map {
         Pair(
@@ -18,8 +23,14 @@ object AirportShuttle {
         )
     }.minByOrNull { it.first }
 
-    data class Bus(
-        val index: Int,
-        val id: Int
-    )
+    fun findEarliestPossibleTime(busses: List<Bus>): Long {
+        var smallestStep = busses[0].id
+        var earliestTime = 0L
+        busses.drop(1).forEach { bus ->
+            while ((earliestTime + bus.index) % bus.id != 0L)
+                earliestTime += smallestStep
+            smallestStep *= bus.id
+        }
+        return earliestTime
+    }
 }
--- src/test/kotlin/aoc2020/day13/AirportShuttleTest.kt
+++ src/test/kotlin/aoc2020/day13/AirportShuttleTest.kt
@@ -15,18 +15,33 @@ class AirportShuttleTest {
 
     @Test
     fun `part 1 test`() {
-        verifyPart1(AirportShuttle.parse(exampleInput), 295)
+        verifyPart1(AirportShuttle.parse(exampleInput), 295L)
     }
 
     @Test
     fun `part 1 real`() {
-        verifyPart1(AirportShuttle.parse(problemInput), 156)
+        verifyPart1(AirportShuttle.parse(problemInput), 156L)
     }
 
-    private fun verifyPart1(timeAndBusses: Pair<Int, List<AirportShuttle.Bus>>, expectedAnswer: Int) {
+    @Test
+    fun `part 2 test`() {
+        verifyPart2(AirportShuttle.parse(exampleInput), 1068781L)
+    }
+
+    @Test
+    fun `part 2 real`() {
+        verifyPart2(AirportShuttle.parse(problemInput), 404517869995362)
+    }
+
+    private fun verifyPart1(timeAndBusses: Pair<Int, List<AirportShuttle.Bus>>, expectedAnswer: Long) {
         val (earliestTime, busses) = timeAndBusses
         val (earliestPossibleTime, bus) = AirportShuttle.findEarliestPossibleBus(earliestTime, busses)!!
         Assertions.assertEquals((earliestPossibleTime - earliestTime) * bus.id, expectedAnswer)
+    }
+
+    private fun verifyPart2(timeAndBusses: Pair<Int, List<AirportShuttle.Bus>>, expectedAnswer: Long) {
+        val earliestPossibleTime = AirportShuttle.findEarliestPossibleTime(timeAndBusses.second)
+        Assertions.assertEquals(earliestPossibleTime, expectedAnswer)
     }
 
 }
